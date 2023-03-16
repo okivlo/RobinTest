ca2a700303dce88275f44e141b066e531626856c
okivlo <i.vloothuis@gmail.com>
2023-03-16 10:58:33 +0100

    Updated main to test for duplicates and for missing values

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from core import data_quality
 import pandas as pd
 import numpy as np
+
+
 def main():
+
+    # This is just a test dataframe from my own project
     df = pd.read_csv('numerical_cat3')
+
+    # Adding a column with missing values
     df['null_column'] = np.NaN
+
+    # Adding a two columns that duplicate each other
+    df['duplicate_column'] = 6
+    df['extra_duplicate_column'] = 6
+
+
     dq = data_quality.DataQuality(df)
     results = dq.evaluate()
+    print(df)
 
 if __name__ == '__main__':
     main()
